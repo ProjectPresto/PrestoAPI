9e9a4621efc9e9f7dfdc952e5269bb34fbe1ccc0
Akasiek <kpomykala2002@gmail.com>
2022-08-24 22:51:25 +0200

    Changed project name to PrestoAPI

--- PowerfulAPI/asgi.py
+++ PowerfulAPI/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for PowerfulAPI project.
+ASGI config for PrestoAPI project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PowerfulAPI.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PrestoAPI.settings")
 
 application = get_asgi_application()
--- manage.py
+++ manage.py
@@ -6,7 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PowerfulAPI.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "PrestoAPI.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
